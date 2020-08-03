b7df33d3620cd3865b5106dd4b297ef0233ee3b1
Jalissa Williams <jalissa_williams@yahoo.com>
2020-08-03 13:08:18 -0400

    Updates

---
+++ precache-manifest.ec83d2822eb46a3295b74204c64c80aa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ba13a2a82c83b8bcf1d84b692bdb364",
+    "url": "/Nutrition/index.html"
+  },
+  {
+    "revision": "b681f8ac238868470179",
+    "url": "/Nutrition/static/css/main.d1ffa3ac.chunk.css"
+  },
+  {
+    "revision": "8961004a6b589eb17a4c",
+    "url": "/Nutrition/static/js/2.e1a0f1bd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Nutrition/static/js/2.e1a0f1bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b681f8ac238868470179",
+    "url": "/Nutrition/static/js/main.33f728f5.chunk.js"
+  },
+  {
+    "revision": "a75ae11a8eadb5d0d1d9",
+    "url": "/Nutrition/static/js/runtime-main.e19ee225.js"
+  },
+  {
+    "revision": "efbd2a8670efcf159bbda99796a09a18",
+    "url": "/Nutrition/static/media/food.efbd2a86.JPG"
+  }
+]);
